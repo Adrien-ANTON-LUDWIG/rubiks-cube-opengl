9b5d06f604bb7d11c949a39f1c1666545cd233e0
Adrien ANTON LUDWIG <adrien.antonludwig@gmail.com>
2022-07-02 18:22:51 +0200

    [FIX] translation warning: int to size_t

--- src/utils/translate.cpp
+++ src/utils/translate.cpp
@@ -1,7 +1,7 @@
 #include "translate.h"
 
 std::vector<GLfloat> translate(std::vector<GLfloat> object, float x, float y, float z) {
-  for (int i = 0; i < object.size(); i++)
+  for (size_t i = 0; i < object.size(); i++)
   {
     object[i] += x;
     object[i + 1] += y;
