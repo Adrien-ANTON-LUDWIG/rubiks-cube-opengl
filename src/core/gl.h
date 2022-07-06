bdf7150f35297e9b85c20f88e8d2c67fd408ad8c
Adrien ANTON LUDWIG <adrien.antonludwig@gmail.com>
2022-07-06 16:25:00 +0200

    [CLEAN] Dead code

--- src/core/gl.h
+++ src/core/gl.h
@@ -39,15 +39,6 @@ void init_textures() {
   glGenerateMipmap(GL_TEXTURE_2D);
   TEST_OPENGL_ERROR();
 
-  // Texture wrapping & filtering options
-  //  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  //  TEST_OPENGL_ERROR();
-  //  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  //  TEST_OPENGL_ERROR();
-  //  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-  //  TEST_OPENGL_ERROR();
-  //  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-  //  TEST_OPENGL_ERROR()
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
